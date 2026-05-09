7a6b5e11216df0be135b1d2ed9ac8fc15ebe6d6d
pvdo <fire_ra@bk.ru>
2026-05-09 09:45:11 +0300

    twoSum using map fast solution

--- twoSum/main.go
+++ twoSum/main.go
@@ -23,12 +23,12 @@ func run() error {
 }
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	m := make(map[int]int)
+	for idx, num := range nums {
+		if v, found := m[target-num]; found {
+			return []int{v, idx}
 		}
+		m[num] = idx
 	}
 	return nil
 }
