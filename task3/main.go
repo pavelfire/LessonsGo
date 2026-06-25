2bd23d7f2035de02221057fa277f9625e7a75c0a
pvdo <fire_ra@bk.ru>
2026-06-25 17:47:45 +0300

    constants

--- task3/main.go
+++ task3/main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
  
+const (
+	a = iota + 1
+	_
+	b
+	c
+	d = c + 2
+	t
+	i
+	i2 = iota + 2
+)
+
 func main() {
+	
     var name string
     var age int
     fmt.Print("Введите имя: ")
@@ -11,4 +23,10 @@ func main() {
     fmt.Scan(&age)
      
     fmt.Println(name, age)
+
+	var a1, b1, c1 int
+	fmt.Scan(&a1, &b1, &c1)
+	fmt.Println(a1, b1, c1)
+
+	fmt.Println("Constants:",a, b, c, d, t, i, i2)
 }
