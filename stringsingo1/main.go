324e198604675d33ed9d4dea73736cf27952baf9
pvdo <fire_ra@bk.ru>
2026-06-25 11:29:17 +0300

    symbol rune string

--- stringsingo1/main.go
+++ stringsingo1/main.go
@@ -5,4 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println(string("Hello Go"[0])) // вывод: 
 	fmt.Println(string(536))
+	fmt.Println("$"[0])
+
+	var symbol int32 = 'c'
+	fmt.Println(string(symbol))  
+	fmt.Println(symbol)
+
+	a:= 10
+	b:=6
+	c:= float64(a) / float64(b)
+	fmt.Println(c)
+
 }
