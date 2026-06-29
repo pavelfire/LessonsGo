613d38d2fca239946e261d9d3b962f7d6d2ee7ea
pvdo <fire_ra@bk.ru>
2026-06-29 09:44:57 +0300

    small tasks

--- task5/main.go
+++ task5/main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	triRaznyeCifri()
+	// triRaznyeCifri()
+	// takeFirstDigit()
+	// threeEqualThree()
+	// isLeapYear()
+	// cycleInGo()
+	// squaresFrom1to10()
+	sumFromFirstToSecondNumber()
 }
 
 func triRaznyeCifri(){
@@ -19,4 +25,70 @@ func triRaznyeCifri(){
 	} else {
 		fmt.Println("NO")
 	}
+}
+
+func takeFirstDigit(){
+    var a int
+    fmt.Scan(&a)
+    str := fmt.Sprintf("%d", a)
+	fmt.Println(string(str[0])) 
+}
+
+func threeEqualThree(){
+	var n int
+	fmt.Scan(&n)
+
+    f1 := n / 100000
+    f2 := (n / 10000) % 10
+    f3 := (n / 1000) % 10
+	d1 := (n / 100) % 10
+	d2 := (n / 10) % 10
+	d3 := n % 10
+
+	if f1 + f2 + f3 == d1 + d2 + d3 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+func isLeapYear(){
+	var y int
+	fmt.Scan(&y)
+
+	if y % 400 == 0 || (y % 4 == 0 && y % 100 != 0) {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+func cycleInGo(){
+	sum := 0
+	// Цикл от 1 до 9
+	for i := 1; i < 10; i++ {
+		fmt.Println(sum)
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func squaresFrom1to10(){
+	for i := 1; i < 11; i++ {
+		fmt.Println(i * i)
+	}
+}
+
+func sumFromFirstToSecondNumber(){
+	var sum int = 0
+    var n1 int
+    var n2 int
+    fmt.Scan(&n1)
+    fmt.Scan(&n2)
+	fmt.Println("--------------------------------")
+	for i := n1; i < n2+1; i++ {
+		fmt.Println(i)
+        sum += i
+	}
+    fmt.Println(sum)
 }
